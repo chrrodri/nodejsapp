a2df3b360d83fd3c039e914b59862c68c53ee09d
Christian Rodriguez <ing.cjair.rodriguez@gmail.com>
2026-05-11 19:36:13 -0600

    Update Git repository and Docker repository names

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js Docker DSL') {
     description('Aplicación Node JS Docker DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/chrrodri/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('chrrodri')
+            node / gitConfigEmail('ing.cjair.rodriguez@gmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('Aplicacion Node.js Docker DSL') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('macloujulian/nodejsapp')
+            repositoryName('chrrodri/nodejsapp')
             tag('${GIT_REVISION,length=7}')
             registryCredentials('docker-hub')
             forcePull(false)
