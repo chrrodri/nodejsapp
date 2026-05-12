2846691daacb472cf36250009651949a529fb5ac
Christian Rodriguez <ing.cjair.rodriguez@gmail.com>
2026-05-11 19:36:40 -0600

    Update Git repository and user configuration

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/chrrodri/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('chrrodri')
+            node / gitConfigEmail('ing.cjair.rodriguez@gmail.com')
         }
     }
     triggers {
